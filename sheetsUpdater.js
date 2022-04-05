d934a5abe50b313534f99701bd4a17f130ef68d9
marikben <70907667+marikben@users.noreply.github.com>
2022-04-05 18:26:38 +0300

    able to show data from bigquery in google sheets

--- sheetsUpdater.js
+++ sheetsUpdater.js
@@ -3,19 +3,14 @@ const {BigQuery} = require('@google-cloud/bigquery');
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/iam','https://www.googleapis.com/auth/bigquery.readonly'];
 const KEYFILEPATH = './credentials.json';
 const gal = require('google-auth-library');
+const { generateAccessToken } = require('./googleSheetsService');
 const auth = new gal.GoogleAuth({
     scopes: SCOPES,
     keyFile: KEYFILEPATH
   });
 const sheets = google.sheets({version: 'v4', auth: auth});
-
-const {
-    generateAccessToken,
-    getSpreadSheet,
-    //getSpreadSheetValues
-  } = require('./googleSheetsService.js');
-
- 
+const spreadsheetId = "14F6fmnBnuB9YtlFfWA9SVyebjyl7emnsovuUUQTktok";
+const authClient = generateAccessToken;
 
 async function main () {
   async function queryBigQuery() {
@@ -25,65 +20,41 @@ async function main () {
         const bigqueryClient = new BigQuery();
 
         // The SQL query to run
-        const sqlQuery = `SELECT tag.description FROM \`nodejsprototype.topset.toptable\` WHERE tag.description = "FA-7403 LIPEÄSÄILIÖ"`;
-
+        const sqlQuery = `SELECT (value, tag, timestamp) FROM \`nodejsprototype.topset.toptable\` WHERE tag.description = "FA-7401 LIPEÄSÄILIÖ" ORDER BY timestamp limit 1`;
+        const sqlQuery2 = `SELECT (value, tag, timestamp) FROM \`nodejsprototype.topset.toptable\` WHERE tag.description = "FA-7403 LIPEÄSÄILIÖ" ORDER BY timestamp limit 1`;
         const options = {
-        query: sqlQuery,
-        // Location must match that of the dataset(s) referenced in the query.
-        //location: 'US',
-        //params: {corpus: 'romeoandjuliet', min_word_count: 250},
+        query: sqlQuery
         };
-
+        const options2 = {
+          query: sqlQuery2
+          };
         // Run the query
-        const [rows] = await bigqueryClient.query(options);
-
-        console.log('Rows:');
-        rows.forEach(row => console.log(row));
+        const rows = await bigqueryClient.query(options);
+        //const rows2 = await bigqueryClient.query(options2);
+        
+        const sheetUpdate = {
+          spreadsheetId: spreadsheetId,
+          auth: auth,
+          valueInputOption: "RAW",
+          range: 'nodejsprotosheet!A2:B2',   
+          resource: { 
+              values: [
+              [rows[0][0].f0_._field_1, rows[0][0].f0_._field_2.description]
+          ]},
+         
+          };
+  
+    try {
+      const response = (await sheets.spreadsheets.values.update(sheetUpdate, spreadsheetId)).data;
+      console.log(JSON.stringify(response, null, 2));
+    } catch (err) {
+      console.error(err);
+    }
     }
 
   queryBigQuery();
   
-  const bigqUpdate = {
-    spreadsheetId: spreadsheetId,
-    auth: authClient,
-    
-    resource : {
-      requests: [{
-      "addDataSource":{
-      "dataSource":{
-         "spec":{
-            "bigQuery":{
-               "projectId":"nodejsprototype",
-               "tableSpec":{
-                  "tableProjectId":"nodejsprototype",
-                  "datasetId":"topset",
-                  "tableId":"toptable"
-               }
-            }
-         }
-      }
-    } }]
-  }
-  };
-
-  const sheetUpdate = {
-        spreadsheetId: spreadsheetId,
-        auth: authClient,
-        valueInputOption: "RAW",
-        range: 'nodejsprotosheet!A2:B2',   
-        resource: { 
-            values: [
-            ["Item", "Wheel"]
-        ]},
-       
-        };
-
-  //try {
-  //  const response = (await sheets.spreadsheets.values.update(sheetUpdate, spreadsheetId)).data;
-  //  console.log(JSON.stringify(response, null, 2));
- // } catch (err) {
-  //  console.error(err);
- // }
+  
 }
 
 main();
